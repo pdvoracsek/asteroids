301911c69e3591d0ce38b7333ccb90909e167ce7
pdvoracsek <pdvoracsek@gmail.com>
2025-01-05 21:55:24 -0500

    added object groups

--- main.py
+++ main.py
@@ -7,24 +7,31 @@ from circleshape import CircleShape
 from player import Player
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     dt = 0
 
-    # Create player
-    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
 
-        player.draw(screen)
-        player.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
+
+        for obj in updatable:
+            obj.update(dt)
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
